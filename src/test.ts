18e9e5b23098480fd8f7ae51d6241603980b8c51
Floris Peters <floris.peters@student.fontys.nl>
2019-01-07 12:33:01 +0100

    edited yml

--- src/test.ts
+++ src/test.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
-/*
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -18,4 +18,3 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
-*/
